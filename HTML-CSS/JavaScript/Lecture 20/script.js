804e6a8c758a7e1ef3d56c7ffadee2f6aa91c00c
PrakharShukla42 <therockstarop9956@gmail.com>
2024-10-10 15:35:21 +0530

    Prakhar

--- HTML-CSS/JavaScript/Lecture 20/script.js
+++ HTML-CSS/JavaScript/Lecture 20/script.js
@@ -11,16 +11,58 @@
 
 
 
-function fun(){
-    var a = 10;
-    var x = ()=>{
-        var b = 20;
+// function fun(){
+//     var a = 10;
+//     var x = ()=>{
+//         var b = 20;
+//         console.log(a);
+//         console.log(b);
+//     }
+//     return x;
+// }
+
+// var p = fun();
+// console.log(p);
+// p();
+
+var a = 20;
+var x = 50;
+
+function outer(){
+    var x = 40;
+    function inner(){
+        var a = 30;
         console.log(a);
-        console.log(b);
+        x++;
+        console.log(x);
     }
-    return x;
+    return inner;
 }
 
-var p = fun();
-console.log(p);
- p();
+var p = outer();
+p();
+
+
+function outerFun(){
+    let money  = 150;
+    function innerFun(){
+        money++;
+        let a = 10;
+        function innerMostFun(){
+            money++;
+            a++;
+            console.log('money : ',money,' a: ',a);
+        }
+        return innerMostFun;
+    }
+    return innerFun;
+}
+let fun = outerFun();
+let fun1 = fun();
+let fun2 = fun();
+
+fun1();
+fun1();
+fun2();
+fun2();
+fun1();
