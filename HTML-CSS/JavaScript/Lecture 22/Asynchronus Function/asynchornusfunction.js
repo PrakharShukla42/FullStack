56174a29235ab22fe2270bf6960e662b2f114457
PrakharShukla42 <therockstarop9956@gmail.com>
2024-10-17 11:11:31 +0530

    Prakhar

---
+++ HTML-CSS/JavaScript/Lecture 22/Asynchronus Function/asynchornusfunction.js
@@ -0,0 +1,18 @@
+console.log('start');
+
+
+// setTimeout(()=>{console.log('mid')}, 3000)
+
+
+// console.log('mid')
+
+// setInterval(()=>{
+//     console.log('somthing')
+// },1000)
+
+
+
+
+
+console.log('end');
+
